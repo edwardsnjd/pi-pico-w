800cabfa45ab94c3eecc3191085707fc59742362
Nicholas Edwards <edwardsnjd@googlemail.com>
2024-04-06 21:50:08 +0800

    feat: Also drive motor forward at low load

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from machine import Pin
+from machine import Pin, PWM
 from time import sleep
 
 led = Pin("LED", Pin.OUT)
 
-on = False
+motor_speed = PWM(Pin(18, Pin.OUT))
+motor_in1 = Pin(17, Pin.OUT)
+motor_in2 = Pin(16, Pin.OUT)
+
+motor_in1.high()
+motor_in2.low()
+motor_speed.freq(22000)
+motor_speed.duty_u16(40000)
+
+led_on = False
 
 while True:
-    on = not on
-    led.value(1 if on else 0)
+    led_on = not led_on
+    led.value(1 if led_on else 0)
     sleep(2)
